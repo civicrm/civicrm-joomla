debf8210ca2def79d6b6b7140eb57ac9f6196db3
Tim Otten <totten@civicrm.org>
2013-02-28 21:49:07 -0800

    Import from SVN (r45945, r596)

---
+++ admin/admin.civicrm.php
@@ -0,0 +1,96 @@
+<?php
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.3                                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('No direct access allowed');
+
+// check for php version and ensure its greater than 5.
+// do a fatal exit if
+if ((int ) substr(PHP_VERSION, 0, 1) < 5) {
+  echo "CiviCRM requires PHP Version 5.2 or greater. You are running PHP Version " . PHP_VERSION . "<p>";
+  exit();
+}
+
+define('CIVICRM_SETTINGS_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'civicrm.settings.php');
+include_once CIVICRM_SETTINGS_PATH;
+
+civicrm_invoke();
+
+function civicrm_init() {
+  require_once 'CRM/Core/ClassLoader.php';
+  CRM_Core_ClassLoader::singleton()->register();
+
+  require_once 'PEAR.php';
+
+  $config = CRM_Core_Config::singleton();
+}
+
+function plugin_init() {
+  //invoke plugins.
+  JPluginHelper::importPlugin('civicrm');
+  $app = JFactory::getApplication();
+  $app->triggerEvent('onCiviLoad');
+}
+
+function civicrm_invoke() {
+  civicrm_init();
+
+  plugin_init();
+
+  $user = JFactory::getUser();
+
+  /* bypass synchronize if running upgrade
+   * to avoid any serious non-recoverable error
+   * which might hinder the upgrade process.
+   */
+
+  if (CRM_Utils_Array::value('task', $_REQUEST) != 'civicrm/upgrade') {
+    CRM_Core_BAO_UFMatch::synchronize($user, FALSE, 'Joomla', 'Individual', TRUE);
+  }
+
+  // Add our standard css & js
+  $resources = CRM_Core_Resources::singleton();
+  $resources->addCoreResources();
+
+  $config = CRM_Core_Config::singleton();
+  if (!$config->userFrameworkFrontend) {
+    $resources->addStyleFile('civicrm', 'css/joomla.css', -97, 'html-header');
+  }
+  else {
+    $resources->addStyleFile('civicrm', 'css/joomla_frontend.css', -97, 'html-header');
+  }
+
+  if (isset($_GET['task'])) {
+    $args = explode('/', trim($_GET['task']));
+  }
+  else {
+    $_GET['task'] = 'civicrm/dashboard';
+    $_GET['reset'] = 1;
+    $args = array('civicrm', 'dashboard');
+  }
+  CRM_Core_Invoke::invoke($args);
+}
+
---
+++ site/civicrm.html.php
@@ -0,0 +1,9 @@
+<?php
+//////////////////////////////////////////////////
+// CiviCRM Front-end Profile - Presentation Layer
+//////////////////////////////////////////////////
+
+defined('_JEXEC') or die('No direct access allowed');
+
+// PUT ALL YOUR FRONT-END HTML CODE HERE...PRESENTATION
+
